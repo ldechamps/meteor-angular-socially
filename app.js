739e0242e0d580e563d3e53cf668e2305cdc415e
ldechamps <ldechamps@hotmail.com>
2016-03-10 00:59:39 +0100

    v1

---
+++ app.js
@@ -0,0 +1,37 @@
+Parties = new Mongo.Collection("parties");
+
+
+if (Meteor.isClient) {
+    angular.module('socially', [
+        'angular-meteor',
+        'ui.router'
+    ]);
+ 
+    angular.module('socially').directive('partiesList', function() {
+        return {
+        restrict: 'E',
+        templateUrl: 'parties-list.html',
+        controllerAs: 'partiesList',
+        controller: function($scope, $reactive) { // Remove the DI manual decleration
+            $reactive(this).attach($scope);
+            
+            this.newParty = {};
+            
+            this.helpers({
+            parties: () => {
+                return Parties.find({});
+            }
+            });
+            
+            this.addParty = () => {
+                Parties.insert(this.newParty);
+                this.newParty = {};
+            };
+            
+            this.removeParty = (party) => {
+                Parties.remove({_id: party._id});
+            }
+        }
+        }
+    });
+}
